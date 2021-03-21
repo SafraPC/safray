66e9ce55e657fb1d84bbcc25b21ff56ef240b34d
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-03-21 14:11:55 -0300

    Drag n drop is working with bugs!

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,48 @@
-import React,{useRef} from 'react';
+import React,{useRef,useContext} from 'react';
 import {useDrag, useDrop} from 'react-dnd';
 import { Container ,Label} from './styles';
+import BoardContext from '../Board/context'
 
-
-function Card({data}) {
+function Card({data,index,listIndex}) {
 
   const ref = useRef();
-
+  const {move} = useContext(BoardContext)
+  
  const [{isDragging}, dragRef] = useDrag({
-   type:"CARD",
+  item:{id:data.id, index,listIndex},
+  type:"CARD",
+   canDrag:true,
    collect:monitor => ({
      isDragging:monitor.isDragging(),
    }),
  })
  const [, dropRef] = useDrop({
   accept:"CARD",
-  hover(){
-    console.log(data.id);
+  hover(item,monitor){
+
+    //log in selected card
+    // console.log(item.index, index);
+    const draggedListIndex = item.listIndex;
+    // const targetListIndex = listIndex;
+
+    const draggedIndex = item.index;
+    const targetIndex = index;
+    
+    if(draggedIndex === targetIndex){
+      return;
+    }
+    const targetSize = ref.current.getBoundingClientRect();
+    const targetCenter = (targetSize.bottom - targetSize.top)/2; 
+    const draggedOffSet = monitor.getClientOffset();
+    const draggedTop = draggedOffSet.y - targetSize.top;
+
+    if(draggedIndex - targetIndex && draggedTop < targetCenter){
+      return;
+    }
+    if(draggedIndex > targetIndex && draggedTop > targetCenter){
+      return
+    }
+    move(draggedListIndex,draggedIndex,targetIndex);
   }
  })
 
