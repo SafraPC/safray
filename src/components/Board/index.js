0c6fef66fc1939de1fbc0c2dd8f65d6a424f8864
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-03-21 14:20:21 -0300

    Finish Base Drag´n drop project.

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -10,12 +10,11 @@ function Board() {
   const [lists,setLists] = useState(data);
   
 
-   function move(fromList,from,to){
-     console.log(fromList,from,to);
+   function move(fromList,toList,from,to){
     setLists(produce(lists,draft =>{
       const dragged = draft[fromList]?.cards[from];
       draft[fromList]?.cards.splice(from,1);
-      draft[fromList]?.cards.splice(to,0,dragged);
+      draft[toList]?.cards.splice(to,0,dragged);
     }))
   }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -23,12 +23,12 @@ function Card({data,index,listIndex}) {
     //log in selected card
     // console.log(item.index, index);
     const draggedListIndex = item.listIndex;
-    // const targetListIndex = listIndex;
+    const targetListIndex = listIndex;
 
     const draggedIndex = item.index;
     const targetIndex = index;
     
-    if(draggedIndex === targetIndex){
+    if(draggedIndex === targetIndex && draggedListIndex === targetListIndex){
       return;
     }
     const targetSize = ref.current.getBoundingClientRect();
@@ -42,8 +42,9 @@ function Card({data,index,listIndex}) {
     if(draggedIndex > targetIndex && draggedTop > targetCenter){
       return
     }
-    move(draggedListIndex,draggedIndex,targetIndex);
+    move(draggedListIndex,targetListIndex,draggedIndex,targetIndex);
     item.index = targetIndex;
+    item.listIndex = targetListIndex;
   }
  })
 
